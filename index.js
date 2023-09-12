39c66ec9f92710208762533c561c2f6ec1b13e3f
MusaKamau <141785937+MusaKamau@users.noreply.github.com>
2023-09-12 23:45:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,27 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 const requestIp = require('request-ip');
-// inside middleware handler
-var ipMiddleware = function(req, res, next) {
-    const clientIp = requestIp.getClientIp(req); 
-    next();
-};
-// on localhost you'll see 127.0.0.1 if you're using IPv4 
-// or ::1, ::ffff:127.0.0.1 if you're using IPv6
-//As Connect Middleware
+
+// As Connect Middleware
 app.use(requestIp.mw())
 
 app.get('/api/whoami', (req, res) => {
-var ipadress = req.clientIp;
-var language = req.acceptsLanguages();
-var software=req.get('User-Agent');
-    res.json({
-      ipadress: ipadress,
-      language:language[0],
-      software:software.slice(software.indexOf("(") + 1, software.indexOf(")"))
-    });
+  const clientIp = req.clientIp;
+  const language = req.acceptsLanguages();
+  const software = req.get('User-Agent');
+  
+  res.json({
+    ipaddress: clientIp, // Corrected to 'ipaddress'
+    language: language[0],
+    software: software.slice(software.indexOf("(") + 1, software.indexOf(")"))
+  });
 });
-
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -43,7 +35,6 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
